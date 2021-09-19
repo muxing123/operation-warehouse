33be32e686c416fde61d1271f7ee68150048c297
muxing123 <1747641424@qq.com>
2021-09-20 00:09:32 +0800

    client修改

--- week03/client.js
+++ week03/client.js
@@ -209,7 +209,7 @@ void async function () {
 
     let viewport = images(800, 600);
 
-    render(viewport, dom.children[0].children[3].children[1].children[3]);
+    render(viewport, dom);
 
     viewport.save("viewport,jpg");
 
